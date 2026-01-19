a413e082250ccf2b1ec4efb5532152ddc3ae7de9
YKJ <vickykat2468@gmail.com>
2026-01-19 16:38:08 +0300

    Rework plate puzzle
    + add musical note sounds to the plates
    + add play of the whole composition when solved

--- Assets/Scripts/Puzzles/PlatePuzzle/PlateController.cs
+++ Assets/Scripts/Puzzles/PlatePuzzle/PlateController.cs
@@ -1,13 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateController : MonoBehaviour
 {
-    [SerializeField] Material m_inactiveMaterial;
-    [SerializeField] Material m_activeMaterial;
-    [SerializeField] Pushable[] m_plates;
+    [SerializeField] private Material m_inactiveMaterial;
+    [SerializeField] private Material m_activeMaterial;
+    [SerializeField] private Pushable[] m_plates;
+    [SerializeField] private AudioSource m_audioSource;
+    [SerializeField] private float m_delay = 0.3f;
 
     private Renderer[] m_renderers;
-    private int[] m_correctSequence = { 1, 1, 3, 5 };
+    private AudioClip[] m_composition;
+    private int[] m_correctSequence = { 1, 1, 3, 5, 1 };
     private int m_currentStep = 0;
     private bool m_isSolved = false;
 
@@ -21,34 +26,46 @@ public class PlateController : MonoBehaviour
         {
             m_renderers[i] = m_plates[i].GetComponent<Renderer>();
         }
+
+        m_composition = new AudioClip[m_correctSequence.Length];
     }
 
     public void AddToSequence(Pushable plate)
     {
+        m_audioSource.PlayOneShot(plate.audioClip);
+
         int correctIndex = m_correctSequence[m_currentStep] - 1;
         if (plate == m_plates[correctIndex])
         {
-            m_renderers[correctIndex].material = m_activeMaterial;
             m_currentStep++;
+
+            for (int i = 0; i < m_composition.Length - 1; i++)
+            {
+                m_composition[i] = m_composition[i + 1];
+            }
+            m_composition[m_composition.Length - 1] = plate.audioClip;
         }
         else
         {
-            ResetPlates();
+            m_currentStep = 0;
             return;
         }
 
         if (m_currentStep == m_correctSequence.Length)
         {
             ChangeMaterials(m_activeMaterial);
-            // TODO Play the complete compostion
+            StartCoroutine(PlaySoundWithDelay());
             m_isSolved = true;
         }
     }
 
-    private void ResetPlates()
+    private IEnumerator PlaySoundWithDelay()
     {
-        ChangeMaterials(m_inactiveMaterial);
-        m_currentStep = 0;
+        foreach(AudioClip audio in m_composition)
+        {
+            m_audioSource.PlayOneShot(audio);
+            yield return new WaitForSeconds(m_delay);
+        }
     }
 
     private void ChangeMaterials(Material material)
--- Assets/Scripts/Puzzles/PlatePuzzle/Pushable.cs
+++ Assets/Scripts/Puzzles/PlatePuzzle/Pushable.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class Pushable : MonoBehaviour
 {
-    
+    [SerializeField] private AudioClip m_audioClip;
+
+    public AudioClip audioClip => m_audioClip;
 }
