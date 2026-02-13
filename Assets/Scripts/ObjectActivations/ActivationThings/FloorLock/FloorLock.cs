dce4701dd5ba85e7719ddb6d341e24436da19388
Egor Konevtsov <egor.konevtsov@gmail.com>
2026-02-13 13:43:11 +0300

    some try

--- Assets/Scripts/ObjectActivations/ActivationThings/FloorLock/FloorLock.cs
+++ Assets/Scripts/ObjectActivations/ActivationThings/FloorLock/FloorLock.cs
@@ -7,22 +7,21 @@ public class FloorLock :Usable
     [SerializeField] Vector3 m_angleRotation = new Vector3(0,0,45);
     [SerializeField] Transform m_rotateObjectl;
     private Rigidbody m_lock;
-    private Rigidbody m_bin;
     public bool m_isUnlocked = false;
 
     private void Awake()
     {
-        m_bin = m_rotateObjectl.GetComponent<Rigidbody>();
+        
         m_lock = GetComponent<Rigidbody>();
         m_lock.isKinematic = true;
-        m_bin.isKinematic = true;
+        
     }
     public override void Use()
     {   
         if (m_isUnlocked) return;
         m_isUnlocked = true;
         m_lock.isKinematic = false;
-        m_bin.isKinematic = false;
+        
         m_rotateObjectl.DORotate(m_angleRotation, m_openTimer);
     }
 }
