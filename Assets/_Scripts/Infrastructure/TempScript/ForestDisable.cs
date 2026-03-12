2b0d4b71f5e813027d905727cc10b72b7795df0d
ciretrus <>
2026-03-12 17:23:38 +0300

    add forest and many other things

---
+++ Assets/_Scripts/Infrastructure/TempScript/ForestDisable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System;
+using System.Collections;
+public class ForestDisable : MonoBehaviour
+{
+    [SerializeField] private TeleportTrigger m_trigger;
+    [SerializeField] private GameObject m_forest;
+
+    private void OnEnable()
+    {
+        m_trigger.Trigger += ForestOff;
+    }
+    private void OnDisable()
+    {
+        m_trigger.Trigger += ForestOff;
+    }
+    public void ForestOff(Action action)
+    {
+        StartCoroutine(Coroutine());
+        
+    }
+
+    private IEnumerator Coroutine()
+    {
+        
+        yield return new WaitForSeconds(5f);
+        m_forest.SetActive(false);
+    }
+
+}
