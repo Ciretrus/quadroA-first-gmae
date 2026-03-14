25e4b23e78bed9e8fc8741a70498bae9206bba91
aNDiQ-CS <nikitin.david2013@yandex.ru>
2026-03-14 23:51:37 +0300

    Add sounds
    + Вальс
    + Рисование рун

---
+++ Assets/_Scripts/Sound/MusicPlatesEasterEgg.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlatesEasterEgg : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_audioSource;    
+    private void OnValidate()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+    }
+
+    public void StartMusic()
+    {
+        m_audioSource.Play();
+    }
+}
