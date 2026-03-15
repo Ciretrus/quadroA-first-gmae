ba260507eb3acac2c53a2e8558ca2e0299af8f0b
ciretrus <>
2026-03-16 01:31:38 +0300

    add easter egg

---
+++ Assets/_Scripts/TestScipts/EasterEgg.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using Puzzles;
+using Puzzles.PortalArch;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+    [SerializeField] private BasePuzzle m_plateController;
+    [SerializeField] private GameObject m_gameObject;
+    [SerializeField] private GameObject m_door;
+    [SerializeField] private Vector3 m_newPos = new Vector3(0,1,1);
+    [SerializeField] private float m_time = 1f;
+    [SerializeField] private AudioSource m_audio;
+    
+
+    private void OnEnable()
+    {
+        m_plateController.Solved += ActivateEgg;
+    }
+    private void OnDisable()
+    {
+        m_plateController.Solved -= ActivateEgg;
+    }
+    public void ActivateEgg()
+    {
+        m_gameObject.SetActive(true);
+        m_door.transform.DOLocalMove(m_newPos, m_time).SetEase(Ease.InQuad);
+        m_audio.Play();
+
+    }
+    
+}
