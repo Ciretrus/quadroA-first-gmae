4624167964fd5351ec3c1062432e002f18601ea8
YKJ <vickykat2468@gmail.com>
2026-03-16 17:54:40 +0300

    Refactor code and folder hierarchy

--- Assets/_Scripts/Puzzles/PlatePuzzle/PlateController.cs
+++ Assets/_Scripts/Puzzles/PlatePuzzle/PlateController.cs
@@ -11,7 +11,6 @@ public class PlateController : BasePuzzle
     [SerializeField] private float m_delay = 0.3f;
 
     private Renderer[] m_renderers;
-    private AudioClip[] m_composition;
     private int m_currentStep = 0;
 
     public bool isSolved => m_isSolved;
@@ -24,22 +23,15 @@ public class PlateController : BasePuzzle
         {
             m_renderers[i] = m_plates[i].GetComponent<Renderer>();
         }
-
-        m_composition = new AudioClip[m_correctSequence.Length];
     }
 
     public void AddToSequence(Pushable plate)
     {        
         int correctIndex = m_correctSequence[m_currentStep] - 1;
+
         if (plate == m_plates[correctIndex])
         {
             m_currentStep++;
-
-            for (int i = 0; i < m_composition.Length - 1; i++)
-            {
-                m_composition[i] = m_composition[i + 1];
-            }
-            //m_composition[m_composition.Length - 1] = plate.audioClip;
         }
         else
         {
@@ -62,11 +54,7 @@ public class PlateController : BasePuzzle
     private IEnumerator WinPuzzle()
     {
         yield return new WaitForSeconds(m_delay + 1);
-        foreach (AudioClip audio in m_composition)
-        {
-            //m_audioSource.PlayOneShot(audio);
-            yield return new WaitForSeconds(m_delay);
-        }
+        
         ChangeMaterials(m_activeMaterial);
     }
 
